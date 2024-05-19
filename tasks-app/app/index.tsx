af092c6ce3b2e4776dc441dcee1712c60ce616ea
Rawan Ahmed <62115135+raawanahmed@users.noreply.github.com>
2024-05-19 14:07:37 +0300

    add styles for the homePage

--- tasks-app/app/index.tsx
+++ tasks-app/app/index.tsx
@@ -1,24 +1,19 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { MyButton } from "@/components/MyButton";
 import React from "react";
-import { Link, useNavigation } from "expo-router";
+import { Link } from "expo-router";
 
 const HomeScreen = () => {
-  const navigation = useNavigation();
-
   return (
     <SafeAreaView style={styles.safeArea}>
-      <Link href="/allTasks">All Tasks</Link>
-      <Link href="/favTasks">Favorite Tasks</Link>
-      {/* <MyButton
-        btnTitle="All Tasks"
-        actionOnPress={() => navigation.navigate("/allTasks")}
-      /> */}
-      {/* <MyButton
-        btnTitle="Fav Tasks"
-        actionOnPress={() => navigation.navigate("/favTasks")}
-      /> */}
+      <View style={styles.container}>
+        <Link href="/allTasks" style={styles.linkText}>
+          All Tasks
+        </Link>
+        <Link href="/favTasks" style={styles.linkText}>
+          Favorite Tasks
+        </Link>
+      </View>
     </SafeAreaView>
   );
 };
@@ -28,5 +23,21 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  container: {
+    width: "80%",
+  },
+  linkText: {
+    padding: 8,
+    borderRadius: 10,
+    marginVertical: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(234, 179, 8, 0.1)",
+    color: "#ca8a04",
+    fontSize: 16,
+    textAlign: "center",
   },
 });
